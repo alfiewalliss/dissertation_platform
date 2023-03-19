0413e1fac5cbd137efcee0b01142ba83aef39a59
alfiewalliss <64089079+alfiewalliss@users.noreply.github.com>
2023-03-19 12:37:16 +0000

    Update settings1.py

--- dissertation_system/dissertation_system/settings1.py
+++ dissertation_system/dissertation_system/settings1.py
@@ -181,7 +181,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "dissertationexchangeinfo@gmail.com"
-EMAIL_HOST_PASSWORD = "unqeawwzndgxhhaq"
+EMAIL_HOST_PASSWORD = 
 DEFAULT_FROM_EMAIL = 'DissertationExchangeTeam <noreply@dissertationexchange.com>'
 
 
